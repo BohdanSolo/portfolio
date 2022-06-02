6ab42487d1a1a5c87bc8e6a355c56704d3e26555
Bohdan <bohdan.solomakha@gmail.com>
2022-06-02 20:09:06 +0300

    Added all files to repo

---
+++ mail.php
@@ -0,0 +1,40 @@
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+//$mail->SMTPDebug = 3;
+
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;
+$mail->Username = 'for.portfolio.sending@gmail.com';
+$mail->Password = 'for.portfolio.sending2022';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+
+$mail->setFrom('for.portfolio.sending@gmail.com');
+$mail->addAddress('solomakha.bohdan@gmail.com');
+//$mail->addAddress('ellen@example.com');
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');
+$mail->isHTML(true);
+
+$mail->Subject = 'Лист з портфоліо';
+$mail->Body    = '' .$name . ' відправив лист, з пошти ' .$email. '<br>Текст листа: ' .$message;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: thank-you.html');
+}
+?>
